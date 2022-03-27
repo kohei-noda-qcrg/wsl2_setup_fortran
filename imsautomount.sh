f04db708fad72790cc8fc82991755c1169c67658
Kohei Noda <kohei-noda@hiroshima-u.ac.jp>
2022-03-28 03:26:44 +0900

    create interactive automout setup.

--- imsautomount.sh
+++ imsautomount.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 set -euo pipefail # Abort script when error is occured.
 cd "$(dirname "$0")"
+ok=abcdefghijklmnopqstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789
+getinput(){
+    read -p "Type your username of ccfep.center.ims.ac.jp :" username
+    checkinput
+}
+checkinput(){
+    case $username in
+        ("") echo -e "Username can't be empty.\nPlease type your username again.\n" && getinput;;
+        (*[!$ok]*) echo -e "contains non-alphabetical or numerical characters.\nPlease type your username again.\n" && getinput;;
+        (*) echo OK
+    esac
+}
 
-# 
-mkdir -p $HOME/sm2
-echo "alias mount='sshfs -o uid=\`id -u\` -o gid=\`id -g\` -o allow_other ims:/home/users/sm2 $HOME/sm2'" >> $HOME/.profile
-echo -e 'if [ $(ls -A $HOME/sm2) ]; then\n\techo "$HOME/sm2 is not empty."\nelse\n\tmount\nfi' >> $HOME/.profile
+getinput
+echo $username
+# Create $HOME/$username directory
+mkdir -p "$HOME/$username"
+# Add alias command to mount ccfep.center.ims.ac.jp:/home/users/$username
+echo "alias mount='sshfs -o uid=\`id -u\` -o gid=\`id -g\` -o allow_other -o nonempty ims:/home/users/$username $HOME/$username'" >> $HOME/.profile
+# Add automount ccfep.center.ims.ac.jp:/home/users/$username script when you launch Ubuntu.
+echo -e "if [ \"\$(ls \$HOME/$username)\" ]; then\n\techo \"\$HOME/$username is not empty.\"\nelse\n\tmount\nfi" >> $HOME/.profile
