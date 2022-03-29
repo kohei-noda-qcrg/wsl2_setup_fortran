d4704ccb135a92dd02885c56eae84511183e9ee5
Kohei Noda <kohei-noda@hiroshima-u.ac.jp>
2022-03-29 10:11:44 +0900

    Add alias named "imsumount" to sudo umount ims mounted directory.

--- imsautomount.sh
+++ imsautomount.sh
@@ -19,6 +19,8 @@ echo $username
 # Create $HOME/$username directory
 mkdir -p "$HOME/$username"
 # Add alias command to mount ccfep.center.ims.ac.jp:/home/users/$username
-echo "alias mount='sshfs -o uid=\`id -u\` -o gid=\`id -g\` -o allow_other -o nonempty ims:/home/users/$username $HOME/$username'" >> $HOME/.profile
+echo "alias mount='sshfs -o uid=\`id -u\` -o gid=\`id -g\` -o allow_other -o nonempty ims:/home/users/$username \$HOME/$username'" >> $HOME/.profile
 # Add automount ccfep.center.ims.ac.jp:/home/users/$username script when you launch Ubuntu.
 echo -e "if [ \"\$(ls \$HOME/$username)\" ]; then\n\techo \"\$HOME/$username is not empty.\"\nelse\n\tmount\nfi" >> $HOME/.profile
+# Add alias command to mount ccfep.center.ims.ac.jp:/home/users/$username
+echo "alias imsumount='sudo umount -f \$HOME/$username'" >> $HOME/.profile
