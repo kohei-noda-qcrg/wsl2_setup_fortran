e4d42f0347b105ccb8b64f201f557159b252e8e5
Kohei Noda <kohei-noda@hiroshima-u.ac.jp>
2022-05-28 21:00:52 +0900

    Support new version modern fotran vscode extension (#10), resolve #9
    * Install a new version Vscode fortran linter extension
    
    * Install fortls instead of fortran-language-server
    
    Because a new version Vscode fortran support extension requires
    foltls instead of fortran-language-server, so that we should install
    fortls.
    
    * Add descriptions about Vscode extensions
    
    * Install editorconfig Vscode support
    
    * Support issue #9
    
    * Support issue #9

--- scripts/vscodeubuntusetup.sh
+++ scripts/vscodeubuntusetup.sh
@@ -10,39 +10,40 @@ echo -e "============================="
 # Install VScode extensions
 ##############################
 
-# WSL
+# WSL and Remote-ssh and Remote-containers support
 code --install-extension ms-vscode-remote.vscode-remote-extensionpack --force
 
 # Utils
-code --install-extension formulahendry.code-runner --force
-code --install-extension streetsidesoftware.code-spell-checker --force
-code --install-extension timonwong.shellcheck --force
-code --install-extension mads-hartmann.bash-ide-vscode --force
-code --install-extension usernamehw.errorlens --force
-code --install-extension PKief.material-icon-theme --force
-code --install-extension IBM.output-colorizer --force
-code --install-extension shardulm94.trailing-spaces --force
-code --install-extension mosapride.zenkaku --force
-code --install-extension sgryjp.japanese-word-handler --force
+code --install-extension formulahendry.code-runner --force # Execute script extension
+code --install-extension streetsidesoftware.code-spell-checker --force # check English Spell
+code --install-extension timonwong.shellcheck --force # Powerful support for syntax error of shell scripts
+code --install-extension mads-hartmann.bash-ide-vscode --force # Powerful support for bash script
+code --install-extension usernamehw.errorlens --force # Whether or not you hover over the error line, highlight errors
+code --install-extension PKief.material-icon-theme --force # Change Vscode explorer icons
+code --install-extension IBM.output-colorizer --force # Colorize output file
+code --install-extension shardulm94.trailing-spaces --force # Detect end of line spaces and highlight these
+code --install-extension mosapride.zenkaku --force # Detect Zenkaku characters and highlight these
+code --install-extension sgryjp.japanese-word-handler --force # Japanese word jump extension
+code --install-extension ms-vscode.hexeditor --force # Binary viewer
+code --install-extension EditorConfig.EditorConfig --force # Force code rules
+
 # C/C++ (required to install Modern Fortran extension)
 code --install-extension ms-vscode.cpptools-extension-pack --force
 
 # Fortran
-code --install-extension ekibun.fortranbreaker --force
-code --install-extension hansec.fortran-ls --force
-code --install-extension krvajalm.linter-gfortran --force
+code --install-extension ekibun.fortranbreaker --force # Fortran breakpoint support
+code --install-extension fortran-lang.linter-gfortran --force # Fortran linter, formatter, syntax highlight
 
 # Git/Github
-code --install-extension mhutchie.git-graph --force
-code --install-extension donjayamanne.githistory --force
-code --install-extension KnisterPeter.vscode-github --force
-code --install-extension GitHub.vscode-pull-request-github --force
-code --install-extension eamodio.gitlens --force
-code --install-extension ms-vscode.hexeditor --force
+code --install-extension mhutchie.git-graph --force # View git history as a graphical tree
+code --install-extension donjayamanne.githistory --force # Add convenient features to Vscode Source control tab
+code --install-extension KnisterPeter.vscode-github --force # Support for github
+code --install-extension GitHub.vscode-pull-request-github --force # Support for github pull requests and issues
+code --install-extension eamodio.gitlens --force # Visualize code authorship and so on
 
-# Markdonw
-code --install-extension yzane.markdown-pdf --force
-code --install-extension DavidAnson.vscode-markdownlint --force
+# Markdown
+code --install-extension yzane.markdown-pdf --force # Convert Markdown to pdf
+code --install-extension DavidAnson.vscode-markdownlint --force # Markdown linter
 
 ##########################################
 # Install vscode-server and launch VScode
@@ -51,7 +52,42 @@ code .
 
 # Create VScode setting file ( for WSL2 distribution virtual matchine )
 mkdir -p "$HOME/.vscode-server/data/Machine"
-which gfortran fortls | awk '{if(NR%2==1){print "{\n\t\"fortran.gfortranExecutable\":\t" "\""$1"\","}else{print "\t\"fortran-ls.executablePath\":\t" "\""$1"\",\n}"}}' > $HOME/.vscode-server/data/Machine/settings.json
+settingfilepath="$HOME/.vscode-server/data/Machine/settings.json"
+if [ -e settingfilepath ]; then
+ cp "$settingfilepath" "$HOME/.vscode-server/data/Machine/settings.json.bak"
+fi
+# Check fortls exsists
+fortlspath=$(command -v fortls)
+if [ "$fortlspath" ]; then
+    fortls=${fortlspath}
+else
+    fortls=${HOME}/.local/bin/fortls
+    print "!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!="
+    print "!!! Warning !!!"
+    print "foltls is not installed on your machine.\nYou should install fortls manually with the following command.\npip install fortls"
+    print "!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!="
+fi
+
+# Write settings.json for Modern Fortran developers
+print "{\n\t\"fortran.linter.compiler\":\t\"gfortran\",\n\t\"fortran.formatting.fprettifyArgs\": [\"-i 4\"],\n\t\"fortran.formatting.formatter\": \"fprettify\",\n\t\"fortran.fortls.path\":\t\"${fortls}\",\n}" > "$settingfilepath"
+
+# Check gfortran exsists
+gfortranpath=$(command -v gfortran)
+if [ ! "$gfortranpath" ]; then
+    print "!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!="
+    print "!!! Warning !!!"
+    print "gfortran is not installed on your machine.\nYou should install gfortran manually with the following command.\nsudo apt install gfortran"
+    print "!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!="
+fi
+# Check gfortran exsists
+fprettifypath=$(command -v fprettify)
+if [ ! "$fprettifypath" ]; then
+    print "!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!="
+    print "!!! Warning !!!"
+    print "fprettify is not installed on your machine.\nYou should install fprettify manually with the following command.\npip install fprettify"
+    print "!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!=!="
+fi
+
 echo -e "============================="
 echo -e "End vscodeubuntusetup.sh"
 echo -e "============================="
